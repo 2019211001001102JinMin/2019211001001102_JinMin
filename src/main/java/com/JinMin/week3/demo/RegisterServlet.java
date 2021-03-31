44d5494ba5573e9f5244c370b431c4180528ccce
2019211001001102JinMin <1767941590@QQ.com>
2021-03-31 20:17:33 +0800

    HW-Week4 - Commit

--- src/main/java/com/JinMin/week3/demo/RegisterServlet.java
+++ src/main/java/com/JinMin/week3/demo/RegisterServlet.java
@@ -1,36 +1,79 @@
 package com.JinMin.week3.demo;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
-//automatic -new --> servlet
+@WebServlet(name = "RegisterServlet", value = "/Register")
+
 public class RegisterServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() {
+        try {
+            Class.forName(getServletConfig().getServletContext().getInitParameter("driver"));
+            con = DriverManager.getConnection(getServletConfig().getServletContext().getInitParameter("url"), getServletConfig().getServletContext().getInitParameter("username"), getServletConfig().getServletContext().getInitParameter("password"));
+            System.out.println("init()--> "+con);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-//request come here- <from method=post>
-        //get parameter from request .
-        String username=request.getParameter("username");//name of input type -<input type="text" name="username"/>
-        String password=request.getParameter("password");//<input type="password" name="password"/>
-        String email=request.getParameter("email");// <input type="text" name="email"/>
-        String gender=request.getParameter("gender");//<input type="radio" name="gender">
-        String birthDate=request.getParameter("birthDate");//<input type="text name=" name="birthDate"/>
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        String gender = request.getParameter("gender");
+        String birthdate = request.getParameter("birthdate");
+        PrintWriter writer = response.getWriter();
+        writer.println("<table border=\"1\">"+"<tr>"+"<td>id</td>"+"<td>username</td>"+"<td>password</td>"+"<td>email</td>"+"<td>gender</td>"+"<td>birthdate</td>"+"</tr>");
 
-    //print - write into response
-        PrintWriter writer = response.getWriter() ;
-        writer.println("<br> username :"+username);
-        writer.println("<br> password :"+password);
-        writer.println("<br> email :"+email);
-        writer.println("<br> gender :"+gender);
-        writer.println("<br> birth Date :"+birthDate);
-        writer.close() ;
 
+        try {
+            Statement createDbStatement = con.createStatement();
+            String insertRequire = "insert into usertable(id,username,password,email,gender,birthdate) values('"+id+"','" + username + "','" + password + "','" + email + "','" + gender + "','" + birthdate + "')";
+            createDbStatement.executeUpdate(insertRequire);
+            String sql = "select * from usertable";
+            ResultSet rs = createDbStatement.executeQuery(sql);
+            while (rs.next()) {
+                id = rs.getString("id");
+                username = rs.getString("username");
+                password = rs.getString("password");
+                email = rs.getString("email");
+                gender = rs.getString("gender");
+                birthdate = rs.getString("birthdate");
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        writer.println("<td>" + id + "</td>" + "<td>" + username + "</td>" + "<td>" + password + "</td>" + "<td>" + email + "</td>" + "<td>" + gender + "</td>" + "<td>" + birthdate + "</td>" + "</tr>");
+
+
+    }
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/main/webapp/Register.jsp
+++ src/main/webapp/Register.jsp
@@ -13,11 +13,12 @@
 <body>
 <form method="post" action="register"><!-- within doPost() in servlet-->
     <font>New User Registration!</font><br/><br/>
+    id<input type="text" name="id"/><br/>
     username<input type="text" name="username"/><br/>
     password<input type="password" name="password"/><br/>
     Email<input type="text" name="email"/><br/>
     Gender:<input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="Female">Female<br/>
-    <input type="date" size="40" name="Date" placeholder="Date of Birth(yyyy-mm-dd)"/><br/>
+    birthdate:<input type="date" size="40" name="birthdate" placeholder="Date of Birth(yyyy-mm-dd)"/><br/>
     <input type="submit" value="Register" />
 
 </form>
